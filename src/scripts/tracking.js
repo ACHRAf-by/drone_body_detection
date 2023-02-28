365629aafa6223cc3b616fcb0d6026281d9c4181
Lucas <lucas.chauvin@gmx.com>
2023-02-28 21:59:41 +0100

    Fixed tracking.js problem for lint

--- src/scripts/tracking.js
+++ src/scripts/tracking.js
@@ -1,21 +1,10 @@
-/**
- * 
- */
 import React from 'react'
 import cocoSsd from "https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"
-import $ from "https://code.jquery.com/jquery-3.3.1.slim.min.js"
+import $ from "jquery"
+import "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"
+import "https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+import "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
 
-<div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js" type="text/javascript"></script>
-
-    
-
-    <script src="js/script.js" defer></script>
-</div>
 
 const demosSection = document.getElementById('demos');
 
