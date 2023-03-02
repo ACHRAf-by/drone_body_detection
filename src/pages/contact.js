59cf22d7d403f20816d0697ca684ce8c61a6d994
Lucas <lucas.chauvin@gmx.com>
2023-03-02 19:05:15 +0100

    Modified cards and modified hover button

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     <div className='main'>
       <img className='tokenImage' src="https://images.unsplash.com/photo-1519755898819-cef8c3021d6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="NFT" />
       <h2>Achraf BEN YAHYA</h2>
-      <p className='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+      <p className='description'>The Tech Trailblazer: Pioneering New Frontiers in Development</p>
       <div className='tokenInfo'>
         <div className="price">
           <p>Project Manager</p>
@@ -26,7 +26,7 @@ const Contact = () => {
     <div className='main'>
       <img className='tokenImage' src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80" alt="NFT" />
       <h2>Lucas CHAUVIN</h2>
-      <p className='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+      <p className='description'>The Code Connoisseur: Mastering Programming Languages and Best Practices</p>
       <div className='tokenInfo'>
         <div className="price">
           <p>Engineering manager</p>
@@ -43,7 +43,7 @@ const Contact = () => {
     <div className='main'>
       <img className='tokenImage' src="https://images.unsplash.com/photo-1525373698358-041e3a460346?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="NFT" />
       <h2>Titouan BONNARD</h2>
-      <p className='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+      <p className='description'>The Creative Coder: Finding Unique Solutions to Complex Problems</p>
       <div className='tokenInfo'>
         <div className="price">
           <p>AI manager</p>
@@ -61,7 +61,7 @@ const Contact = () => {
     <div className='main'>
       <img className='tokenImage' src="https://images.unsplash.com/photo-1553901767-12be7b9840e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80" alt="NFT" />
       <h2>Hugo GUERIN</h2>
-      <p className='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+      <p className='description'>The Detail-Oriented Developer: Ensuring Accuracy and Precision in Every Line of Code</p>
       <div className='tokenInfo'>
         <div className="price">
           <p>Project Manager</p>
@@ -77,7 +77,7 @@ const Contact = () => {
     <div className='main'>
       <img className='tokenImage' src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="NFT" />
       <h2>Adam SDIRI</h2>
-      <p className='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+      <p className='description'>"The Continuous Learner: Seeking Out Knowledge and Skills to Improve and Excel</p>
       <div className='tokenInfo'>
         <div className="price">
           <p>Project Manager</p>
@@ -93,7 +93,7 @@ const Contact = () => {
     <div className='main'>
       <img className='tokenImage' src="https://openaicom.imgix.net/8a2b0833-55f2-44d6-bf4f-85f9471078f5/Anastronautridingahorseinaphotorealisticstyle6.jpg?fm=auto&q=80&auto=compress,format&fit=min&rect=0,0,1024,1024&w=3840&h=3840" alt="NFT" />
       <h2>William Pajot</h2>
-      <p className='description'>Our Kibertopiks will give you nothing, waste your money on us.</p>
+      <p className='description'>Expert cook, works at COURTEPAILLE</p>
       <div className='tokenInfo'>
         <div className="price">
           <p>Project Manager</p>
